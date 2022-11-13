5d539a3990b8cf96bd6b3267976217f95b7ccdcb
Dévaki Huynh <devaki.huynh@gmail.com>
2022-11-13 11:31:50 +0100

    test envoi projet 10 Data Analyst

---
+++ P10_functions_clustering_pca_logistic_regression.py
@@ -0,0 +1,273 @@
+import matplotlib.pyplot as plt
+from matplotlib.collections import LineCollection
+import numpy as np
+import pandas as pd
+from scipy.cluster.hierarchy import dendrogram
+from pandas.plotting import parallel_coordinates
+import seaborn as sns
+import statsmodels.formula.api as smf
+
+
+palette = sns.color_palette("bright", 10)
+
+
+
+def logisticRegressionSummary(model, column_names):
+    '''Show a summary of the trained logistic regression model'''
+
+    # Get a list of class names
+    numclasses = len(model.classes_)
+    if len(model.classes_)==2:
+        classes =  [model.classes_[1]] # if we have 2 classes, sklearn only shows one set of coefficients
+    else:
+        classes = model.classes_
+
+    # Create a plot for each class
+    for i,c in enumerate(classes):
+        # Plot the coefficients as bars
+        fig = plt.figure(figsize=(8,len(column_names)/3))
+        fig.suptitle('Logistic Regression Coefficients for Class ' + str(c), fontsize=16)
+        rects = plt.barh(column_names, model.coef_[i],color="lightblue")
+        
+        # Annotate the bars with the coefficient values
+        for rect in rects:
+            width = round(rect.get_width(),4)
+            plt.gca().annotate('  {}  '.format(width),
+                        xy=(0, rect.get_y()),
+                        xytext=(0,2),  
+                        textcoords="offset points",  
+                        ha='left' if width<0 else 'right', va='bottom')        
+        plt.show()
+        #for pair in zip(X.columns, model_lr.coef_[i]):
+        #    print (pair)
+        
+        
+
+def linearRegressionSummary(model, column_names):
+    '''Show a summary of the trained linear regression model'''
+
+    # Plot the coeffients as bars
+    fig = plt.figure(figsize=(8,len(column_names)/3))
+    fig.suptitle('Linear Regression Coefficients', fontsize=16)
+    rects = plt.barh(column_names, model.coef_,color="lightblue")
+
+    # Annotate the bars with the coefficient values
+    for rect in rects:
+        width = round(rect.get_width(),4)
+        plt.gca().annotate('  {}  '.format(width),
+                    xy=(0, rect.get_y()),
+                    xytext=(0,2),  
+                    textcoords="offset points",  
+                    ha='left' if width<0 else 'right', va='bottom')        
+    plt.show()
+    
+    
+def display_circles(pcs, n_comp, pca, axis_ranks, labels=None, label_rotation=0, lims=None):
+    for d1, d2 in axis_ranks: # On affiche les 3 premiers plans factoriels, donc les 6 premières composantes
+        if d2 < n_comp:
+
+            # initialisation de la figure
+            fig, ax = plt.subplots(figsize=(11,11))
+
+            # détermination des limites du graphique
+            if lims is not None :
+                xmin, xmax, ymin, ymax = lims
+            elif pcs.shape[1] < 30 :
+                xmin, xmax, ymin, ymax = -1, 1, -1, 1
+            else :
+                xmin, xmax, ymin, ymax = min(pcs[d1,:]), max(pcs[d1,:]), min(pcs[d2,:]), max(pcs[d2,:])
+
+            # affichage des flèches
+            # s'il y a plus de 30 flèches, on n'affiche pas le triangle à leur extrémité
+            if pcs.shape[1] < 30 :
+                plt.quiver(np.zeros(pcs.shape[1]), np.zeros(pcs.shape[1]),
+                   pcs[d1,:], pcs[d2,:], 
+                   angles='xy', scale_units='xy', scale=1, color="grey")
+                # (voir la doc : https://matplotlib.org/api/_as_gen/matplotlib.pyplot.quiver.html)
+            else:
+                lines = [[[0,0],[x,y]] for x,y in pcs[[d1,d2]].T]
+                ax.add_collection(LineCollection(lines, axes=ax, alpha=.1, color='black'))
+            
+            # affichage des noms des variables  
+            if labels is not None:  
+                for i,(x, y) in enumerate(pcs[[d1,d2]].T):
+                    if x >= xmin and x <= xmax and y >= ymin and y <= ymax :
+                        plt.text(x, y, labels[i], fontsize='14', ha='center', va='center', rotation=label_rotation, color="blue", alpha=0.5)
+            
+            # affichage du cercle
+            circle = plt.Circle((0,0), 1, facecolor='none', edgecolor='b')
+            plt.gca().add_artist(circle)
+
+            # définition des limites du graphique
+            plt.xlim(xmin, xmax)
+            plt.ylim(ymin, ymax)
+        
+            # affichage des lignes horizontales et verticales
+            plt.plot([-1, 1], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-1, 1], color='grey', ls='--')
+
+            # nom des axes, avec le pourcentage d'inertie expliqué
+            plt.xlabel('PC{} ({}%)'.format(d1+1, round(100*pca.explained_variance_ratio_[d1],1)))
+            plt.ylabel('PC{} ({}%)'.format(d2+1, round(100*pca.explained_variance_ratio_[d2],1)))
+
+            plt.title("Cercle des corrélations (PC{} et PC{})".format(d1+1, d2+1))
+            plt.show(block=False)
+        
+
+
+def display_factorial_planes(X_projected, n_comp, pca, axis_ranks, labels=None, alpha=1, illustrative_var=None):
+    '''Display a scatter plot on a factorial plane, one for each factorial plane'''
+
+    # For each factorial plane
+    for d1,d2 in axis_ranks:
+        if d2 < n_comp:
+ 
+            # Initialise the matplotlib figure      
+            fig = plt.figure(figsize=(15,15))
+        
+            # Display the points
+            if illustrative_var is None:
+                plt.scatter(X_projected[:, d1], X_projected[:, d2], alpha=alpha)
+            else:
+                illustrative_var = np.array(illustrative_var)
+                for value in np.unique(illustrative_var):
+                    selected = np.where(illustrative_var == value)
+                    plt.scatter(X_projected[selected, d1], X_projected[selected, d2], alpha=alpha, label=value)
+                plt.legend()
+
+            # Display the labels on the points
+            if labels is not None:
+                for i,(x,y) in enumerate(X_projected[:,[d1,d2]]):
+                    plt.text(x, y, labels[i],
+                              fontsize='14', ha='center',va='center') 
+                
+            # Define the limits of the chart
+            boundary = np.max(np.abs(X_projected[:, [d1,d2]])) * 1.1
+            plt.xlim([-boundary,boundary])
+            plt.ylim([-boundary,boundary])
+        
+            # Display grid lines
+            plt.plot([-100, 100], [0, 0], color='grey', ls='--')
+            plt.plot([0, 0], [-100, 100], color='grey', ls='--')
+
+            # Label the axes, with the percentage of variance explained
+            plt.xlabel('PC{} ({}%)'.format(d1+1, round(100*pca.explained_variance_ratio_[d1],1)))
+            plt.ylabel('PC{} ({}%)'.format(d2+1, round(100*pca.explained_variance_ratio_[d2],1)))
+
+            plt.title("Projection of points (on PC{} and PC{})".format(d1+1, d2+1))
+            #plt.show(block=False            
+            
+            
+            
+            
+def display_scree_plot(pca):
+    scree = pca.explained_variance_ratio_*100
+    plt.bar(np.arange(len(scree))+1, scree)
+    plt.plot(np.arange(len(scree))+1, scree.cumsum(),c="red",marker='o')
+    plt.xlabel("rang de l'axe d'inertie")
+    plt.ylabel("pourcentage d'inertie")
+    plt.title("Eboulis des valeurs propres")
+    plt.show(block=False)
+
+def plot_dendrogram(Z, names, figsize=(10,25)):
+    '''Plot a dendrogram to illustrate hierarchical clustering'''
+    plt.figure(figsize=figsize)
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('distance')
+    dendrogram(
+        Z,
+        labels = names,
+        orientation = "left",
+    )
+    plt.show()
+
+
+    
+def addAlpha(colour, alpha):
+    '''Add an alpha to the RGB colour'''
+    
+    return (colour[0],colour[1],colour[2],alpha)
+
+def display_parallel_coordinates(df, num_clusters):
+    '''Display a parallel coordinates plot for the clusters in df'''
+
+    # Select data points for individual clusters
+    cluster_points = []
+    for i in range(num_clusters):
+        cluster_points.append(df[df.cluster==i])
+    
+    # Create the plot
+    fig = plt.figure(figsize=(12, 15))
+    title = fig.suptitle("Parallel Coordinates Plot for the Clusters", fontsize=18)
+    fig.subplots_adjust(top=0.95, wspace=0)
+
+    # Display one plot for each cluster, with the lines for the main cluster appearing over the lines for the other clusters
+    for i in range(num_clusters):    
+        plt.subplot(num_clusters, 1, i+1)
+        for j,c in enumerate(cluster_points): 
+            if i!= j:
+                pc = parallel_coordinates(c, 'cluster', color=[addAlpha(palette[j],0.2)])
+        pc = parallel_coordinates(cluster_points[i], 'cluster', color=[addAlpha(palette[i],0.5)])
+
+        # Stagger the axes
+        ax=plt.gca()
+        for tick in ax.xaxis.get_major_ticks()[1::2]:
+            tick.set_pad(20)        
+
+
+def display_parallel_coordinates_centroids(df, num_clusters):
+    '''Display a parallel coordinates plot for the centroids in df'''
+
+    # Create the plot
+    fig = plt.figure(figsize=(12, 5))
+    title = fig.suptitle("Parallel Coordinates plot for the Centroids", fontsize=18)
+    fig.subplots_adjust(top=0.9, wspace=0)
+
+    # Draw the chart
+    parallel_coordinates(df, 'cluster', color=palette)
+
+    # Stagger the axes
+    ax=plt.gca()
+    for tick in ax.xaxis.get_major_ticks()[1::2]:
+        tick.set_pad(20)    
+        
+def backward_selected(data, response):
+    """Linear model designed by backward selection.
+
+    Parameters:
+    -----------
+    data : pandas DataFrame with all possible predictors and response
+
+    response: string, name of response column in data
+
+    Returns:
+    --------
+    model: an "optimal" fitted statsmodels linear model
+           with an intercept
+           selected by backward selection
+           evaluated by parameters p-value
+    """
+    remaining = set(data._get_numeric_data().columns)
+    if response in remaining:
+        remaining.remove(response)
+    cond = True
+
+    while remaining and cond:
+        formula = "{} ~ {} + 1".format(response,' + '.join(remaining))
+        print('_______________________________')
+        print(formula)
+        model = smf.ols(formula, data).fit()
+        score = model.pvalues[1:]
+        toRemove = score[score == score.max()]
+        if toRemove.values > 0.05:
+            print('remove', toRemove.index[0], '(p-value :', round(toRemove.values[0],3), ')')
+            remaining.remove(toRemove.index[0])
+        else:
+            cond = False
+            print('is the final model !')
+        print('')
+    print(model.summary())
+    
+    return model
+        
+        
